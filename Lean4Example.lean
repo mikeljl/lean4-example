88d4fbadc58810b42bc4e9ba5dc02beddd1ab346
Mike Lu <101956851+mikeljl@users.noreply.github.com>
2025-04-16 16:07:20 -0700

    up

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -18,11 +18,15 @@ theorem hello_world (a b c : Nat)
   : a + b + c = a + c + b := by
   rw [add_assoc, add_comm b, ←add_assoc]
 
+namespace TESTTEST
+
 theorem foo (a : Nat) : a + 1 = Nat.succ a := by rfl
 
 variable {G : Type*} [Group G]
 
 theorem inv_mul_self' (a : G) : a⁻¹ * a = 1 := inv_mul_cancel a
+
+end TESTTEST
 
 theorem left_inverse_of_element (a : G) : a⁻¹ * a = 1 := by
   -- Step 1: Recall the definition of the inverse in a group
