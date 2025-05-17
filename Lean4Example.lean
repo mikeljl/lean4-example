feff33e7adb2fb9c00e33c82255316b7977acaae
Mike Lu <101956851+mikeljl@users.noreply.github.com>
2025-05-17 16:07:13 +0800

    test

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -4,4 +4,4 @@ theorem hello_world (a b c : Nat)
   : a + b + c = a + c + b := by
   rw [add_assoc, add_comm b, ←add_assoc]
 
-theorem foo (a : Nat) : a + 1 = Nat.succ a := by rfl
+private theorem foo (a : Nat) : a + 1 = Nat.succ a := by rfl
